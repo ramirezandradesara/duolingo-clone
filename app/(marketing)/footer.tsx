c2b49a26210e6bfe70dae4d09701a7923eda57a8
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-04-03 23:46:55 -0300

    style footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 const Footer = () => {
+  const flags = [
+    { src: "/hr.svg", name: "Croatian" },
+    { src: "/es.svg", name: "Spanish" },
+    { src: "/it.svg", name: "Italian" },
+    { src: "/fr.svg", name: "French" },
+    { src: "/jp.svg", name: "Japanese" },
+  ];
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        {flags.map(({ src, name }, index) => (
+          <Button size={"lg"} variant={"ghost"} className="w-full" key={index}>
+            <Image
+              src={src}
+              alt={name}
+              width={32}
+              height={40}
+              className="mr-4 rounded-md"
+            />
+            {name}
+          </Button>
+        ))}
+      </div>
     </footer>
   );
 };

08eff7190433942b1a509f0ca0a6b4b97e708a2c
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-04-08 21:40:23 -0300

    add layout for learn pages

---
+++ components/sidebar.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+type Props = {
+  className?: string;
+};
+const Sidebar = ({ className }: Props) => {
+  return (
+    <div
+      className={cn(
+        "bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        className
+      )}
+    >
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;

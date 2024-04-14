2c3a32456c470b1ef0cb2c0ff60b0be1b3526e00
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-04-14 17:38:05 -0300

    create sidebar items

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,11 @@
 import { cn } from "@/lib/utils";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import SidebarItem from "./sidebar-item";
+import { ClerkLoading, ClerkLoaded, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+
 type Props = {
   className?: string;
 };
@@ -7,11 +13,40 @@ const Sidebar = ({ className }: Props) => {
   return (
     <div
       className={cn(
-        "bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+        "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
         className
       )}
     >
-      Sidebar
+      <Link href={"/learn"}>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src={"/mascot.svg"} alt={"logo"} height={40} width={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Duo clone
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem label={"learn"} iconSrc={"/learn.svg"} href={"/learn"} />
+        <SidebarItem
+          label={"Leaderboard"}
+          iconSrc={"/leaderboard.svg"}
+          href={"/leaderboard"}
+        />
+        <SidebarItem
+          label={"Quests"}
+          iconSrc={"/quests.svg"}
+          href={"/quests"}
+        />
+        <SidebarItem label={"Shop"} iconSrc={"/shop.svg"} href={"/shop"} />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };

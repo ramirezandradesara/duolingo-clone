34e9b1eba8927f75f8f681e009a07f5e24b78acf
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-04-16 00:29:20 -0300

    create learn page wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,26 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWapper from "@/components/sticky-wrapper";
 import React from "react";
+import Header from "./header";
+import UserProgress from "@/components/user-progress";
 
+// ? fle-row-reverse may be unuseful
 const LearnPage = () => {
-  return <div>LearnPage</div>;
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWapper>
+        <UserProgress
+          activeCourse={{ title: "spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />{" "}
+      </StickyWapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;

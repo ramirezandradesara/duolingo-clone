a6cb6dcc77285345d221e102490623d6f7503aa5
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2024-04-27 00:40:48 -0300

    fix typo in files names

--- db/dizzle.ts
+++
@@ -1,9 +0,0 @@
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "./schema";
-
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
-const db = drizzle(sql, { schema }); // prisma alternative
-
-export default db;
-// const result = await db.select().from(...);
--- db/schema.ts
+++ db/schema.ts
@@ -3,5 +3,5 @@ import { pgTable, serial, text } from "drizzle-orm/pg-core";
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
-  imageSrc: text("iamge_src").notNull(),
+  imageSrc: text("image_src").notNull(),
 });
